5b9f36e549b4e6ae67383020a1ab2ec01ac9cf8a
Vasja Volin <vasja.volin@gmail.com>
2020-08-26 17:21:22 -0700

    change styling a bit to make space

--- src/frontend/src/components/Main.vue
+++ src/frontend/src/components/Main.vue
@@ -2,23 +2,24 @@
   <div class="main">
     <div class="container px-4 sm:px-8 lg:px-16 xl:px-20 mx-auto">
       <div class="grid grid-cols-1 md:grid-cols-12 gap-8 items-center">
-        <div class="hero-text col-span-6 text-left">
+        <div class="hero-text col-start-1 col-span-5 text-left">
           <h1 class="font-bold text-4xl md:text-5xl max-w-xl text-gray-900 leading-tight">
             Skill Challenge Calculator
           </h1>
           <hr class="w-12 h-1 bg-orange-500 rounded-full mt-8">
-          <p class="text-gray-800 text-base leading-relaxed mt-8 font-semibold">
-            Put in your party's skill bonus, skill check DC, successes, and failures for the
-            Skill Challenge you'd like to run. The calculator will tell you how likely your party
+          <p class="text-gray-800 text-base leading-relaxed mt-8 font-medium">
+            Select your party's skill bonus, skill check DC, successes, and failures for the
+            <a class='text-blue-400' href='https://www.youtube.com/watch?v=GvOeqDpkBm8'>Skill Challenge</a>
+            you'd like to run. The calculator will tell you how likely your party
             is to succeed the challenge.
           </p>
-          <p class="text-gray-800 text-base leading-relaxed mt-8 font-semibold">
+          <p class="text-gray-800 text-base leading-relaxed mt-8 font-medium">
             You should approximate your party's average skill bonus - it may be hard to
             calculate the exact chance of success for parties with
             extremely varied skill bonuses.
           </p>
         </div>
-        <div class="hero-image col-span-6">
+        <div class="hero-image col-start-7 col-span-6">
           <form @change="formSubmit">
             <div class="mb-4">
               <label class="label" for="avg_skill_bonus" >Average Skill Bonus</label >
