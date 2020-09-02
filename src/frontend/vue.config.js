8a97e3fb4ed15d17600a7e5eee44ef630aab1d33
Vasja Volin <vasja.volin@gmail.com>
2020-09-01 23:57:12 -0700

    try to update the config

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  publicPath: process.env.BASE_URL
+  publicPath: process.env.BASE_URL,
+
+  // So we don't get an 'Invalid Host' error message when we
+  // try to run this in production mode. Notes here: https://bit.ly/3lI7sig
+  devServer: {
+    public: 'skillcalc5e.cc'
+  }
 }
