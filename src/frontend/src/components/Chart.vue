16099a51b814415aaa8e6979faca4b50a218aae5
Vasja Volin <vasja.volin@gmail.com>
2020-09-25 14:33:57 -0700

    adjust the chart to look good

--- src/frontend/src/components/Chart.vue
+++ src/frontend/src/components/Chart.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <h2>Chart</h2>
     <svg id='viz' width='400' height='400' class='container-border' />
   </div>
 </template>
@@ -15,75 +14,118 @@ export default {
   mounted () {
     const data = [
       {
+        successes: 3,
+        percent: 50.0,
+      },
+      {
         successes: 4,
-        percent: 50.5
+        percent: 34.3,
       },
       {
         successes: 5,
-        percent: 30.2
+        percent: 22.6
       },
       {
         successes: 6,
-        percent: 25.1
+        percent: 14.4
       },
       {
         successes: 7,
-        percent: 20.6
-      }
+        percent: 8.9
+      },
+      {
+        successes: 8,
+        percent: 5.4
+      },
+      {
+        successes: 9,
+        percent: 3.2
+      },
+      {
+        successes: 10,
+        percent: 1.9
+      },
+      {
+        successes: 11,
+        percent: 1.1
+      },
     ]
 
-    const width = 500
-    const height = 500
-    const svg = d3
-      .select('#viz')
-      .attr('viewBox', [-10, -10, width, height])
-      .attr('font-family', 'sans-serif')
-      .attr('font-size', 10);
+    const svg = d3.select('#viz')
+                  .attr('font-family', 'sans-serif')
+                  .attr('font-size', 10);
+
+    const margin = { top: 10, right: 10, bottom: 70, left: 80 }
+    const width = +svg.attr('width') - margin.left - margin.right
+    const height = +svg.attr('height') - margin.top - margin.bottom
+
+    // we want a central piece to draw on that's translated properly
+    const g = svg.append("g")
+                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     const x = d3.scalePoint()
-                .domain(data.map(d => d.successes))
-                .range([0, 400])
+                .domain([3, 4, 5, 6, 7, 8, 9, 10, 11])
+                .range([0, width])
 
     const y = d3.scaleLinear()
                 .domain([100, 0])
-                .range([0, 400])
+                .range([0, height])
 
     const line = d3.line()
                    .x(d => x(d.successes))
                    .y(d => y(d.percent))
 
-    svg
-      .append('path')
-      .attr('d', line(data))
-      .attr('fill', 'none')
-      .attr('stroke', 'rgb(0 0 0)')
-      .attr('stroke-width', 1);
+    const xAxisCall = d3.axisBottom(x)
+    const xAxis = g.append("g")
+                   .attr("class", "x-axis")
+                   .attr("transform", "translate(" + 0 + "," + (height + 20) + ")")
+                   .call(xAxisCall);
+
+    const yAxisCall = d3.axisLeft(y)
+    const yAxis = g.append("g")
+                   .attr("class", "y-axis")
+                   .attr("transform", "translate(" + -30 + "," + 0 + ")")
+                   .call(yAxisCall)
+
+    xAxis.append("text")
+         .attr("transform", "translate(" + (width - 23) + ", 0)")
+         .attr("y", -6)
+         .text("Successes")
+         .attr("fill", "currentColor")
+
+    yAxis.append("text")
+         .attr("transform", "rotate(-90)")
+         .attr("y", 16)
+         .text("Overall Success Chance")
+         .attr("fill", "currentColor")
+
+    g.append('path')
+     .attr('d', line(data))
+     .attr('fill', 'none')
+     .attr('stroke', 'rgb(0 0 0)')
+     .attr('stroke-width', 1)
 
     // these are blank spots on the chart to provide a background for
     // the text below to be added and readable
-    svg
-      .append("g")
-      .attr("fill", "#fff")
-      .selectAll("circle")
-      .data(data)
-      .join("circle")
-      .attr("cx", d => x(d.successes))
-      .attr("cy", d => y(d.percent))
-      .attr('stroke', 'rgb(0 0 0)')
-      .attr('stroke-width', '1')
-      .attr("r", 12);
+    g.append("g")
+     .attr("fill", "#fff")
+     .selectAll("circle")
+     .data(data)
+     .join("circle")
+     .attr("cx", d => x(d.successes))
+     .attr("cy", d => y(d.percent))
+     .attr("r", 12)
 
     // this is the percentages as text on the chart
-    svg
-      .append('g')
-      .attr('text-anchor', 'middle')
-      .selectAll('text')
-      .data(data)
-      .join('text')
-      .attr('x', d => x(d.successes))
-      .attr('y', d => y(d.percent))
-      .attr('dy', '0.35em')
-      .text(d => d.percent);
+    g.append('g')
+     .attr('text-anchor', 'middle')
+     .selectAll('text')
+     .data(data)
+     .join('text')
+     .attr('x', d => x(d.successes))
+     .attr('y', d => y(d.percent))
+     .attr('dy', '0.35em')
+     .text(d => d.percent)
   }
 }
 </script>
