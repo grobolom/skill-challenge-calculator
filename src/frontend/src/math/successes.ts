f708d25fd1e25879a1e3cda07bf82b0cc8be3ba1
Vasja Volin <vasja.volin@gmail.com>
2020-10-02 23:16:54 -0700

    add comments, add some cleanup

--- src/frontend/src/math/successes.ts
+++ src/frontend/src/math/successes.ts
@@ -9,6 +9,9 @@ function factorial(n: number): number {
   return f[n] = factorial(n-1) * n;
 }
 
+// This is a complicated function that's derived from the binomial
+// distribution. It is used to calculate the odds of succeeding on a specific
+// trial, given a total number of successes.
 function calculateSingleProbability(baseProbability: number, successes: number, trials: number): number {
   const firstTerm = factorial(trials - 1) / (factorial(successes - 1) * factorial(trials - successes))
   const secondTerm = Math.pow(baseProbability, successes)
@@ -17,6 +20,11 @@ function calculateSingleProbability(baseProbability: number, successes: number, 
   return firstTerm * secondTerm * thirdTerm
 }
 
+// Since the binomial probability calculation above only tackles specific trial
+// instances (e.g. the probability of having 6 successes on the 8th trial), this
+// function is needed to sum up the values for the whole range of failures and
+// successes (e.g. the probability of getting the 6th success on the 6th, 7th,
+// or 8th trial, for 6 successes and 3 failures as inputs)
 function calculateProbability(baseProbability: number, successes: number, failures: number) {
   let sum = 0;
 
@@ -27,10 +35,22 @@ function calculateProbability(baseProbability: number, successes: number, failur
   return sum
 }
 
-export default function getSuccesses(skillBonus: number, checkDC: number, failures: number): { successes: number; probability: number }[] {
+// Handles calculating probabilities for successes. Output is something like:
+// [ { successes: 3, probability: 0.5 }, ... ]
+// Currently uses a static list of successes to calculate for. The probability is
+// calculated using the negative binomial distribution
+export default function getSuccesses(
+    skillBonus: number,
+    checkDC: number,
+    failures: number
+  ): { successes: number; probability: number }[] {
+
+  // the base probability for success is based on the character's skill bonus
+  // compared to the check DC. A bonus of 5 and a DC of 16 will give a 50% chance
+  // of success (0-10 fails, 11-20 succeeds).
   const baseProbability = (21 - (checkDC - skillBonus)) / 20;
 
-  // just defining this here for convenience
+  // just defining a constant here for convenience, but this could be expanded
   const successesRange = [3, 4, 5, 6, 7, 8, 9, 10, 11]
 
   return successesRange.map((value): { successes: number; probability: number } => {
--- src/frontend/src/store/index.ts
+++ src/frontend/src/store/index.ts
@@ -13,23 +13,25 @@ export default createStore({
     decrementSkillBonus (state) {
       state.skillBonus = (state.skillBonus == 0) ? 0 : state.skillBonus - 1
     },
+    setSkillBonus (state, newValue: number) {
+      state.skillBonus = (newValue < 0) ? 0 : newValue
+    },
+
     incrementCheckDC (state) {
       state.checkDC++
     },
     decrementCheckDC (state) {
       state.checkDC = (state.checkDC == 0) ? 0 : state.checkDC - 1
     },
+    setCheckDC (state, newValue: number) {
+      state.checkDC = (newValue < 0) ? 0 : newValue
+    },
+
     incrementFailures (state) {
       state.failures++
     },
     decrementFailures (state) {
       state.failures = (state.failures == 0) ? 0 : state.failures - 1
-    },
-    setSkillBonus (state, newValue: number) {
-      state.skillBonus = (newValue < 0) ? 0 : newValue
-    },
-    setCheckDC (state, newValue: number) {
-      state.checkDC = (newValue < 0) ? 0 : newValue
     },
     setFailures (state, newValue: number) {
       state.failures = (newValue < 0) ? 0 : newValue
@@ -42,23 +44,25 @@ export default createStore({
     decrementSkillBonus({ commit }) {
       commit('decrementSkillBonus')
     },
+    setSkillBonus({ commit }, newValue) {
+      commit('setSkillBonus', newValue)
+    },
+
     incrementCheckDC({ commit }) {
       commit('incrementCheckDC')
     },
     decrementCheckDC({ commit }) {
       commit('decrementCheckDC')
     },
+    setCheckDC({ commit }, newValue) {
+      commit('setCheckDC', newValue)
+    },
+
     incrementFailures({ commit }) {
       commit('incrementFailures')
     },
     decrementFailures({ commit }) {
       commit('decrementFailures')
-    },
-    setSkillBonus({ commit }, newValue) {
-      commit('setSkillBonus', newValue)
-    },
-    setCheckDC({ commit }, newValue) {
-      commit('setCheckDC', newValue)
     },
     setFailures({ commit }, newValue) {
       commit('setFailures', newValue)
