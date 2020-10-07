41eb7c9237fa3bf0f2fe3b101265de721c040a8f
Vasja Volin <vasja.volin@gmail.com>
2020-10-06 23:40:36 -0700

    remove prerender - too complex in Docker atm

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -18,26 +18,5 @@ module.exports = {
       entry: 'src/main.ts',
       title: '5e Skill Challenge Calculator'
     }
-  },
-
-  // there might be a better way to handle this in the future, (Vue 3 launch)
-  // but this sets up pre-rendering the site for SEO
-  chainWebpack: config => {
-    config
-        .plugin("prerender-spa-plugin")
-        .use(PrerenderSpaPlugin)
-        .init(Plugin => new Plugin({
-            staticDir: path.join(__dirname, 'dist'),
-            routes: ['/'],
-            postProcess(renderedRoute) {
-              renderedRoute.html = renderedRoute.html.replace(`id="app"`, `id="app" data-server-rendered="true"`)
-              return renderedRoute
-            },
-            renderer: new Renderer({
-              inject: {},
-              headless: true,
-              renderAfterElementExists: '#main',
-          })
-        }))
   }
 }
